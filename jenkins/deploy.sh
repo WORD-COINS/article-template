cceb40ec76e067149eb0e0aec00824cc32dc79ff
Yoshimura Yuu <m@yyu.pw>
2017-03-30 16:07:13 +0900

    add deploy script to word server

---
+++ jenkins/deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# smbのWORD-SERVERをマウントして、特定のフォルダにPDFファイルを投入します。
+# WORD-SEVERのIPアドレスとログインユーザー、パスワードは環境変数で与えられます。
+# また、PDFを設置する場所も環境変数で与えられます。
+#
+# $WORD_SEVER_IPADDR WORD-SERVERのIPアドレス
+# $WORD_LOGIN_USER   WORD-SEVERのログインユーザー
+# $WORD_LOGIN_PASS   WORD-SERVERのログインパスワード
+# $article_filename  PDFの名前
+# $article_directory 記事のフォルダ
+# $branch            Git Branch名
+# $dist              コピー先のフォルダ（例: WORD記事/2017年度/2017 43号/01 未赤入れ）
+
+set -e
+
+# Checkout the branch
+git checkout "$branch"
+git submodule update --init --recursive
+
+# Build
+cd "articles/$artcle_directory"
+WORD_FONT=hiragino-pron make
+
+# Monunt WORD-SEVER
+mount_smbfs "smb://${WORD_LOGIN_USER}:${WORD_LOGIN_PASS}@${WORD_SERVER_IPADDR}/WORD-ARTICLES/" /Volumes/WORD-ARTICLES
+
+# Copy the PDF
+cp main.pdf "/Volumes/WORD-ARTICLES$dist/$article_filename"
+
+# Unmount
+unmount /Volumes/WORD-ARTICLES
