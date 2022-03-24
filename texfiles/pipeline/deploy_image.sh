d8bc00c7aa26da48ef721d5fbca011f7a8924127
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2022-03-25 04:52:59 +0900

    remove unnecessary files

--- texfiles/pipeline/deploy_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-if [[ "$BITBUCKET_BRANCH" == "master" ]]; then
-  export IMAGE_NAME=wordcoins/texfiles
-  docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
-  docker push "${IMAGE_NAME}:latest"
-fi
